118ab377e48288ad114499b3362832ea6e0b7d06
pd130 <parthn.dhande@gmail.com>
2026-07-12 18:45:13 +0530

    Add files via upload

---
+++ Arrays/283. Move Zeroes.py
@@ -0,0 +1,37 @@
+#Brute Force 1
+# Time Complexity : O(N)
+# Space Complexity : O(N)
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        zero_count = nums.count(0)
+        nums[:] = [x for x in nums if x != 0]
+        nums += [0]*zero_count
+        
+        
+
+                
+#Brute Force 2
+#Time Complexity : (worsrt case) O(N^2)
+#Space Complexity : O(1)
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        zero_count = nums.count(0)
+        for i in range(len(nums)-1 , -1 , -1):
+            if nums[i] == 0:
+                nums.pop(i)
+        nums += [0]*zero_count
+        
+        
+# Optimal 
+# Time Complexity : O(N)
+# Space Complexity : O(1)
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[k] , nums[i] = nums[i] , nums[k]
+                i += 1
+                k += 1
+            else:
+                i+=1
